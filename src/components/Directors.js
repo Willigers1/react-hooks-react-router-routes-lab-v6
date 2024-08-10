82d4ac89a50efd6fe6814371ef2f485c3db14dbf
Willigers <ronowilligers@gmail.com>
2024-08-10 16:44:45 +0300

    complete lab

---
+++ src/components/Directors.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { directors } from "../data";
+
+function Directors() {
+  const getDirectors = directors.map((director) => (
+    <div key={director.name}>
+      {director.name}
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {getDirectors}
+    </div>
+  );
+}
+
+export default Directors;
